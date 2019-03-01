de0c984212ba898425002b5c02024c6711fdbb44
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2019-03-01 19:16:54 +0300

    Git Initialization

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '11.0'
+
+use_frameworks!
+
+target 'iTunesSearch' do
+
+	pod 'Alamofire', '~> 5.0.0-beta.2'
+    pod 'Kingfisher', '~> 5.0'
+	pod 'RxSwift',    '~> 4.0'
+    pod 'RxCocoa',    '~> 4.0'
+    
+end
---
+++ iTunesSearch/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by Ulaş Sancak on 1.03.2019.
+//  Copyright © 2019 Ulaş Sancak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
